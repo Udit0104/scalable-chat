f84d885048c3aea174652d31293e50f16d1bc372
udit0104 <uditverma145@gmail.com>
2026-01-09 21:10:14 +0530

    first commit

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,32 @@
+import { log } from 'console';
+import {Server} from 'socket.io'
+
+class SocketService{
+    private _io:Server;
+    constructor(){
+        console.log('Init Socket Service');
+        this._io=new Server({
+            cors:{
+                allowedHeaders:["*"],
+                origin:"*",
+            },
+        });
+    }
+
+    public initListeners(){
+        const io=this.io;
+        console.log("Init Socket Listeners...");
+        io.on('connect',async socket=>{
+            console.log("New socket connected",socket.id);
+            socket.on('event:message',async({message}:{message:string})=>{
+                console.log("New Message Rec.", message);
+            })
+            
+        })
+    }
+
+    get io(){
+        return this._io;
+    }
+}
+export default SocketService;
---
+++ apps/web/context/SocketProvider.tsx
@@ -0,0 +1,42 @@
+'use client'
+import React,{useCallback,useEffect,useContext} from 'react'
+import {io} from "socket.io-client"
+interface SocketProviderProps{
+    children?: React.ReactNode
+}
+
+interface ISocketContext{
+    sendMessage:(msg:string)=>any;
+}
+
+const SocketContext=React.createContext<ISocketContext | null>(null)
+
+export const useSocket=()=>{
+    const state = useContext(SocketContext)
+    if(!state) throw new Error(`state is undefined`)
+
+    return state;
+}
+
+export const SocketProvider:React.FC<SocketProviderProps>=({children})=>{
+    const sendMessage: ISocketContext['sendMessage'] = useCallback((msg)=>{
+        console.log('Send Message',msg);
+    },[]);
+
+    useEffect(()=>{
+        const _socket=io('http://localhost:8000')
+        return ()=>{
+            _socket.disconnect();
+        };
+    },[])
+
+    return (
+        <SocketContext.Provider value={{sendMessage}}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
+
+// function useCallback(arg0: (msg: any) => void, arg1: never[]): (msg: string) => any {
+//     throw new Error('Function not implemented.');
+// }
